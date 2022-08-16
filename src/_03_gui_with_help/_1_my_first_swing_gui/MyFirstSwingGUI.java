e59bebd5278fb064c47a29affc049b1387e6e897
root <root@8dcb98300915>
2022-08-16 01:25:50 +0000

    photo url thing

--- src/_03_gui_with_help/_1_my_first_swing_gui/MyFirstSwingGUI.java
+++ src/_03_gui_with_help/_1_my_first_swing_gui/MyFirstSwingGUI.java
@@ -23,25 +23,27 @@ jFrame.setVisible(true);
 		// small.
 
 		// 4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
 		// 5. Create and initialize an object of the JPanel class
-
+JPanel jPanel = new JPanel();
+jPanel.setVisible(true);
 		// 6. Create and initialize an object of the JLabel class
-
+JLabel jLabel = new JLabel();
+jLabel.setVisible(true);
 		// 7. Set the text of the JLabel to a lovely greeting.
-
+jLabel.setText("hi");
 		// 8. Add the JPanel object to the JFrame
-
+jFrame.getContentPane().add(jPanel);
 		// 9. Add the JLabel object to the JPanel
-
+jPanel.add(jLabel);
 		// 10. Pack your JFrame.
-
+jFrame.pack();
 		// 11. Run your program again. Do you see your message.
 
 		// 12. Use the loadImage method to set the icon of the JLabel object.
-
+loadImage();
 		// 13. Re-pack the JFrame object.
-
+jFrame.pack();
 		// 14. Run the program one more time. Do you see the image?
 
 	}
